46c4edd10842a533e6449e052c166cff4f457f0c
Work <work@HomeMacPro.local>
2024-07-03 22:19:14 +0530

    Add profile for postgres db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS countries (
+CREATE TABLE IF NOT EXISTS Country (
     id INT PRIMARY KEY,
     name VARCHAR(50)
 );
 
-INSERT INTO countries (id, name) VALUES (1, 'USA');
-INSERT INTO countries (id, name) VALUES (2, 'France');
-INSERT INTO countries (id, name) VALUES (3, 'Brazil');
-INSERT INTO countries (id, name) VALUES (4, 'Italy');
-INSERT INTO countries (id, name) VALUES (5, 'Canada');
+INSERT INTO Country (id, name) VALUES (1, 'USA');
+INSERT INTO Country (id, name) VALUES (2, 'France');
+INSERT INTO Country (id, name) VALUES (3, 'Brazil');
+INSERT INTO Country (id, name) VALUES (4, 'Italy');
+INSERT INTO Country (id, name) VALUES (5, 'Canada');
