92daae2bd3d5d0bbbd9fb2c252b9354cd84d44fe
Work <work@HomeMacPro.local>
2024-07-05 06:57:19 +0530

    Add config for initializing db from data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,12 @@
+TRUNCATE TABLE Country;
+
 CREATE TABLE IF NOT EXISTS Country (
     id INT PRIMARY KEY,
     name VARCHAR(50)
 );
 
-INSERT INTO Country (id, name) VALUES (1, 'USA');
-INSERT INTO Country (id, name) VALUES (2, 'France');
-INSERT INTO Country (id, name) VALUES (3, 'Brazil');
-INSERT INTO Country (id, name) VALUES (4, 'Italy');
-INSERT INTO Country (id, name) VALUES (5, 'Canada');
+INSERT INTO Country (id, name) VALUES (100, 'USA');
+INSERT INTO Country (id, name) VALUES (200, 'UAE');
+INSERT INTO Country (id, name) VALUES (300, 'Switzerland');
+INSERT INTO Country (id, name) VALUES (400, 'Turkey');
+INSERT INTO Country (id, name) VALUES (500, 'Japan');
