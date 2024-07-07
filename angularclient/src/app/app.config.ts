1e549df89596155b88e01051f5b74741bfc268e1
Work <bhidek@ae.com>
2024-07-07 18:00:22 +0530

    Add Info Component in UI

--- angularclient/src/app/app.config.ts
+++ angularclient/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { routes } from './app.routes';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { CountryService } from './country-service.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), CountryService]
 };
--- angularclient/src/app/app.routes.ts
+++ angularclient/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CountryListComponent } from './country-list/country-list.component';
+import { InfoComponent } from './info/info.component';
 
 export const routes: Routes = [
   { path: 'country-list', component: CountryListComponent },
   { path: 'countries', component: CountryListComponent },
+  { path: 'info', component: InfoComponent }
 ];
 
 
