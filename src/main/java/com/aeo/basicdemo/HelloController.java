dab1d11b56cebbd067c60d03969fe5668627ad9e
Work <bhidek@ae.com>
2024-07-07 17:44:27 +0530

    Add CrossOrigins for Angular UI and json response to HelloController

--- src/main/java/com/aeo/basicdemo/HelloController.java
+++ src/main/java/com/aeo/basicdemo/HelloController.java
@@ -1,19 +1,21 @@
 package com.aeo.basicdemo;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import netscape.javascript.JSObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 @RestController
 @Tag(name = "Hello Controller", description = "This is Hello Controller")
 @Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloController {
 
     @GetMapping("/")
@@ -24,6 +26,16 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return InfoMessages.HELLO;
+    }
+
+    @GetMapping("/json")
+    public Map<String, String> json() {
+        Map<String, String> infoMap = new HashMap<>();
+        infoMap.put("name", "BasicDemo");
+        infoMap.put("description", "A basic repo with Java, SpringBoot, H2 DB and AngularJS");
+        infoMap.put("updated", "07-Jul-2024");
+        infoMap.put("message", "Corrected errors");
+        return infoMap;
     }
 
     private String getVersion() {
