e5e631d13a8272f73d672cfcedfb763ed9bc8469
KaumudiB <bhidek@ae.com>
2024-07-08 23:52:18 +0530

    minor changes

--- src/main/java/com/aeo/basicdemo/countries/CountryController.java
+++ src/main/java/com/aeo/basicdemo/countries/CountryController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @Tag(name = "Country Controller", description = "This service controls country operations")
 @Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
 public class CountryController {
 
     @Autowired
